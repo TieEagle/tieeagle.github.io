32db41ab5f5628b0737958d4c66eac2954e754fd
Daniel Schock <daniel.schock@glinnex.com>
2021-01-15 13:26:39 +0100

    Added sass styles fix

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withSass = require("@zeit/next-sass");
+const withFonts = require("next-fonts");
+
+
+// noinspection JSUnusedLocalSymbols
+module.exports = withFonts(
+  withSass({
+    webpack(config, options) {
+      return config;
+    }
+  })
+);
